b94e699192673b6c8fdfc5fe6ba7f1395dc70453
ShaRaNzp <sharan@live.cl>
2019-11-25 01:30:56 -0300

    Lo Podemos Lograaaaar

--- SAE/SAE/settings.py
+++ SAE/SAE/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vop_r)-s8!af7ef4ll!y@emb@*mb@)v!#o=4z@gd!7vg-#nv-r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
