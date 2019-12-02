376baf65261d17fbec3993de93a64f1d44c32b55
kingbayron <you@example.com>
2019-12-02 19:32:40 -0300

    arreglo error multiselectfield

--- SAE/Registro/models.py
+++ SAE/Registro/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from django.db.models.signals import post_save
-from multiselectfield import MultiSelectField
 
 class Colegio(models.Model):
 	nombre=models.CharField(max_length=20)
